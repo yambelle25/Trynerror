c1c506301ba1b10a3a33e38caabd10da73426f4b
yambelle25 <maryam.nabilah010125@gmail.com>
2024-09-18 16:25:47 +0800

    This is the first class of java

---
+++ calculation.java
@@ -0,0 +1,44 @@
+public class calculation {
+
+    private String name;
+    private int age;
+    private int yos;
+    private double gpa1;
+    private double gpa2;
+    private double gpa3;
+    
+
+
+    public calculation(String name, int age,int yos, double gpa1,double gpa2,double gpa3){
+        this.name = name;
+        this.age = age;
+        this.yos = yos;
+        this.gpa1 = gpa1;
+        this.gpa2 = gpa2;
+        this.gpa3 = gpa3;
+
+    }
+    
+    public void displayDetails(){
+        int maxInt = Integer.MAX_VALUE;
+        long maxPlusOne = 2147483648L;
+        boolean condition = false;
+        System.out.println(maxPlusOne);
+        System.out.println(maxInt);
+        System.out.println("The Name is :" + " " + name);
+        System.out.println("The Age is :" +" "+ age);
+        System.out.println("The Gpa is :" +" "+ gpa1);
+        System.out.println("The Gpa is :" +" "+ gpa2);
+        System.out.println("The Gpa is :" +" "+ gpa3);
+        System.out.println("The Average of Gpa :" + ((gpa1+gpa2+gpa3)/3));
+        System.out.println("The Years of study is :" +" "+ yos);
+        
+
+
+    }
+
+    public static void main(String[] args) {
+        calculation calculation = new calculation("naim" , 27,3,3,4,3);
+        calculation.displayDetails();
+    }
+}
