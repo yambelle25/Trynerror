99c97fbdacd0445e4d42eda79e2eab7b12c90e42
yambelle25 <maryam.nabilah010125@gmail.com>
2024-09-19 09:18:04 +0800

    This is the first class of java

---
+++ Olc.java
@@ -0,0 +1,10 @@
+public class Olc {
+    public static void main(String[] args) {
+        String obj1 = new String("Object 1");
+    }
+    
+    obj1= new String ("Object 2");
+
+    System.out.println(obj1);
+
+}
