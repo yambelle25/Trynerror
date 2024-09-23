2d7167ec8196934ff9b3449c1843a99775bf9683
yambelle25 <maryam.nabilah010125@gmail.com>
2024-09-23 14:28:03 +0800

    exercise

---
+++ twodimension.java
@@ -0,0 +1,27 @@
+public class twodimension {
+    public static void main (String[]args){
+        int[][] matrix = new int[3][4];
+
+        matrix [0][0]=1;matrix [0][1]=2; matrix [0][2]=3;matrix [0][3]=4;
+        matrix [1][0]=5;matrix [1][1]=6; matrix [1][2]=7;matrix [1][3]=8;
+        matrix [2][0]=9;matrix [2][1]=10; matrix [2][2]=11;matrix [2][3]=12;
+        
+       
+        
+        int[][] arrays={
+            {1,2,3,4},
+            {5,6,7,8},
+            {9,10,11,12}
+        };
+      //  for (int row = 0; row<matrix.length; row++){
+       //     for (int col=0; col < matrix [row].length;col++);
+      //      System.out.println(matrix[row][col]);
+       // }
+
+        for (int[] array:arrays){
+            for (int number : array){
+                System.out.println(number);
+            }
+        }
+    }
+}
